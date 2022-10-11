f44d8f5961a2f504fd67f26aa7115418ecd87031
nsn1930 <pavel.brodovskij@gmail.com>
2022-10-11 17:38:46 +0200

    partial header css

--- coinrate/src/components/Header.jsx
+++ coinrate/src/components/Header.jsx
@@ -2,8 +2,15 @@ import logo from '../assets/logo.svg';
 
 function Header() {
     return (
-        <div className='header'>
-            <img className='logoCoin' src={logo} alt="logo" />
+        <div className="headerContainer">
+            <div className='header'>
+                <img className='logoCoin' src={logo} alt="logo" />
+                <div className='headerLinks'>
+                    <a href=".">Chrome extention</a>
+                    <a href=".">Price compartion</a>
+                    <a href=".">Blog</a>
+                </div>
+            </div>
         </div>
     );
 }
