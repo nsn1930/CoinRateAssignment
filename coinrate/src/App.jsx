2bdc968b866656889009f29c579fdfe6bef27f11
nsn1930 <pavel.brodovskij@gmail.com>
2022-10-11 18:04:29 +0200

    main container basic html and css

--- coinrate/src/App.jsx
+++ coinrate/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header';
+import MainContainer from './components/MainContainer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <MainContainer></MainContainer>
     </div>
   );
 }
---
+++ coinrate/src/components/MainContainer.jsx
@@ -0,0 +1,14 @@
+// import logo from '../assets/logo.svg';
+
+function MainContainer() {
+    return (
+        <div className="mainContainer">
+            <div className="mainColumns">
+                <div className="leftColumn"></div>
+                <div className="rightColumn"></div>
+            </div>
+        </div>
+    );
+}
+
+export default MainContainer;
